46f689574ead314025d04ae219658b24cf8ef26f
Klavs Taube <klavs@fullcontact.com>
2016-12-08 13:19:49 +0200

    add random mutations

--- dev/user.clj
+++ dev/user.clj
@@ -1,6 +1,25 @@
 (ns user
-  (:require [full.core.config :as conf]
-            [full.core.log :as log]))
+  (:require [full.core.config :refer [opt] :as conf]
+            [full.core.log :as log]
+            [full.async :refer [go-try]]
+            [proxinius.api :as api]
+            [proxinius.mutations :as mutations]))
 
 (conf/configure)
 (log/configure)
+
+(def mutation-percentage (opt :mutation-percentage :default 10))
+
+(defn should-mutate? [_]
+  (>= @mutation-percentage (rand-int 100)))
+
+(defn do-random-mutation> [request]
+  (go-try
+   (let [mutation (rand-nth mutations/mutations)]
+     (log/info "Mutating request"
+               mutation
+               (:uri request)
+               (:headers request))
+     (mutation request @api/requests))))
+
+(api/add-request-mutation should-mutate? do-random-mutation>)
