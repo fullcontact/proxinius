1dd35e988a83c7ccea2bf77f70021392295ee168
Karlis Lauva <k@karlis.me>
2016-12-23 11:53:29 +0200

    add EPL license

--- project.clj
+++ project.clj
@@ -2,6 +2,9 @@
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [fullcontact/full.bootstrap "0.10.3"]]
   :main proxinius.api
+  :license {:name "Eclipse Public License - v 1.0"
+            :url "http://www.eclipse.org/legal/epl-v10.html"
+            :distribution :repo}
   :resource-paths ["resources"]
   :target-path "target/%s"
   :plugins [[lein-bikeshed "0.3.0"]
