ad9c9811717bb1cd0303e9423114db54e9bf0423
Klavs Taube <klavs@fullcontact.com>
2017-02-10 15:13:28 +0200

    add user/add-route-mutation

--- dev/user.clj
+++ dev/user.clj
@@ -3,7 +3,8 @@
             [full.core.log :as log]
             [full.async :refer [go-try]]
             [proxinius.api :as api]
-            [proxinius.mutations :as mutations]))
+            [proxinius.mutations :as mutations]
+            [clout.core :refer [route-matches]]))
 
 (conf/configure)
 (log/configure)
@@ -21,5 +22,12 @@
                (:uri request)
                (:headers request))
      (mutation request @api/requests))))
+
+(defn add-route-mutation
+  "`route` - string in clout route format
+  https://github.com/weavejester/clout
+  `mutation` - function that accepts request and returns a channel"
+  [route mutation]
+  (api/add-request-mutation #(route-matches route %) mutation))
 
 (api/add-request-mutation should-mutate? do-random-mutation>)
